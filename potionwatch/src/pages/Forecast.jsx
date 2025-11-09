0e5cddccca905818d9e31f448c101b2c2f05ad46
Trung Pham <dinhtrungpham125@gmail.com>
2025-11-09 07:47:03 -0600

    Add OptimizationPlan component to Forecast page

--- potionwatch/src/pages/Forecast.jsx
+++ potionwatch/src/pages/Forecast.jsx
@@ -4,6 +4,7 @@ import OptimizedRoutes from "../components/forecast/OptimizedRoutes";
 import DebugPanel from "../components/forecast/DebugPanel";
 import MinimumWitches from "../components/forecast/MinimumWitches";
 import DailySchedule from "../components/forecast/DailySchedule";
+import OptimizationPlan from "../components/OptimizationPlan";
 import {
   requireJson,
   buildPredictions,
@@ -230,6 +231,8 @@ export default function Forecast() {
       />
 
       {/* Sections */}
+      <OptimizationPlan />
+
       <MinimumWitches
         data={minimumWitches}
         loading={loadingForecast}
